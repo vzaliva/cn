24cde5a13cea679d117ff41a736b8c69e2f2ce5c
Valerii Huhnin <olympichek1@gmail.com>
2025-03-11 05:02:45 +0200

    work in progress on `SCtypes` decidability

--- coq/Cn/SCtypes.v
+++ coq/Cn/SCtypes.v
@@ -1,6 +1,10 @@
+Require Import Coq.Structures.DecidableType.
+Require Import Coq.Structures.DecidableTypeEx.
+Require Import Coq.Program.Equality.
 Require Import Cerberus.Symbol.
 Require Import Cerberus.IntegerType.
 Require Import Cerberus.Ctype.
+Require Import Sym.
 (* Qualifiers *)
 Definition qualifiers := Ctype.qualifiers.
 
@@ -29,3 +33,55 @@ Record c_concrete_sig := mk_c_concrete_sig {
   sig_variadic : bool;
   sig_has_proto : bool
 }.
+
+Module IntegerBaseType_as_MiniDecidableType <: MiniDecidableType.
+  Definition t := integerBaseType.
+  Definition eq := @eq t.
+  Lemma eq_dec : forall (x y : t), { eq x y } + { ~ eq x y }.
+  Proof.
+    unfold eq.
+    intros x y.
+    destruct x, y.
+    all: try (right; discriminate).
+    all: try (left; reflexivity).
+    all: decide equality.
+    all: apply Nat_as_DT.eq_dec.
+  Qed.
+End IntegerBaseType_as_MiniDecidableType.
+
+Module IntegerType_as_MiniDecidableType <: MiniDecidableType.
+  Definition t := integerType.
+  Definition eq := @eq t.
+  Lemma eq_dec : forall (x y : t), { eq x y } + { ~ eq x y }.
+  Proof.
+    unfold eq.
+    intros x y.
+    destruct x, y.
+    all: try (right; discriminate).
+    all: try (left; reflexivity).
+    all: decide equality.
+    all: try apply IntegerBaseType_as_MiniDecidableType.eq_dec.
+    all: try apply Sym_t_as_MiniDecidableType.eq_dec.
+  Qed.
+End IntegerType_as_MiniDecidableType.
+
+Module qualifiers_as_MiniDecidableType <: MiniDecidableType.
+  Definition t := qualifiers.
+  Definition eq := @eq t.
+  Lemma eq_dec : forall (x y : t), { eq x y } + { ~ eq x y }.
+  Proof.
+    unfold eq.
+    intros x y.
+    destruct x, y.
+    do 2 decide equality.
+  Qed.
+End qualifiers_as_MiniDecidableType.
+
+Module SCtypes_as_MiniDecidableType <: MiniDecidableType.
+  Definition t := t.
+  Definition eq := @eq t.
+  Lemma eq_dec : forall (x y : t), { eq x y } + { ~ eq x y }.
+  Proof.
+    admit.
+  Admitted.
+End SCtypes_as_MiniDecidableType.
