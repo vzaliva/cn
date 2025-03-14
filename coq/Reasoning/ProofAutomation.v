49078a17c27c55fbd83d42302f4ed7de36ea4341
Vadim Zaliva <lord@crocodile.org>
2025-03-14 14:28:00 -0700

    Resource matching checks via `provable`.

--- coq/Reasoning/ProofAutomation.v
+++ coq/Reasoning/ProofAutomation.v
@@ -80,8 +80,8 @@ Qed.
           (* pointereq/\iargseq *)
           (Control.focus 1 1 (fun () =>
            Std.split false NoBindings;         
-           Control.focus 1 1 (Std.reflexivity);
-           Control.focus 1 1 (Std.reflexivity)
+           Control.focus 1 1 (Control.shelve);  (* TODO: add pointer equality proof (via provable) *)
+           Control.focus 1 1 (Control.shelve)  (* TODO: add argument equality proof (via provable) *)
          )))
      | [] =>
          Control.throw (Tactic_failure (Some (Message.of_string "No resource change between the input and output")))
