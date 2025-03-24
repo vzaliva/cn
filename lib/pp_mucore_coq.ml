64decc6839712fadf5b17f2c71ca2198e3bd5936
Vadim Zaliva <lord@crocodile.org>
2025-03-24 14:04:59 -0700

    indent

--- lib/pp_mucore_coq.ml
+++ lib/pp_mucore_coq.ml
@@ -2198,7 +2198,7 @@ let pp_unfold_step (s : Prooflog.unfold_step) =
 
 
 let rec pp_log_entry = function
-  | Prooflog.PredicateRequest (c, s, p, r, h,c') ->
+  | Prooflog.PredicateRequest (c, s, p, r, h, c') ->
     pp_constructor
       "PredicateRequest"
       [ pp_context c;
@@ -2211,11 +2211,8 @@ let rec pp_log_entry = function
   | Prooflog.UnfoldResources (c, loc, steps, c') ->
     pp_constructor
       "UnfoldResources"
-      [ pp_context c;
-        pp_location loc;
-        pp_list pp_unfold_step steps;
-        pp_context c'
-      ]
+      [ pp_context c; pp_location loc; pp_list pp_unfold_step steps; pp_context c' ]
+
 
 let coq_steps_var_name = "ResourceInferenceSteps"
 
